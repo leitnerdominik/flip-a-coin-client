9aee96341a3b97acbf0b1f3379dafc949bef3ad9
Dominik Leitner <bigtoasti@gmail.com>
2019-06-14 11:18:36 +0200

    adjust axios config

--- src/axios-coin.js
+++ src/axios-coin.js
@@ -2,12 +2,8 @@ import axios from 'axios';
 
 require('dotenv').config();
 
-const baseURL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:8080/'
-    : process.env.SERVER_NAME;
+const baseURL = process.env.SERVER_NAME;
 
-console.log(process.env.NODE_ENV, baseURL);
 
 const instance = axios.create({
   baseURL,
